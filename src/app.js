6ba6eb9cf845e89d129f6801834ca1c5e67aeead
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-13 19:45:34 +0600

    added error handler for no api end point match

--- src/app.js
+++ src/app.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const app = express();
 const tourRouter = require("./routes/tourRoute");
 
+// middlewares
 app.use(express.json());
 app.use(cors());
 
+// routes
 app.use("/api/v1/tours", tourRouter);
+
+// error handler for no api endpoint match
+app.all("*", function (req, res, next) {
+  res
+    .status(404)
+    .json({
+      status: "fail",
+      message: `Can't find ${req.originalUrl} on this server`,
+    });
+});
 
 module.exports = app;
