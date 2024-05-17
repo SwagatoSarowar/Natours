07d2e41f4b83640792ae19fbb36b2de8301242b4
SwagatoSarowar <swagotosarowar1@gmail.com>
2024-05-17 10:42:22 +0600

    error handling modified (incomplete)

--- src/utils/appError.js
+++ src/utils/appError.js
@@ -5,6 +5,8 @@ const AppError = class extends Error {
     this.statusCode = statusCode;
     this.status = String(statusCode).startsWith("4") ? "fail" : "error";
     this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor)
   }
 };
 
